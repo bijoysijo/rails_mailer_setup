c83a1fa7661a9ad5ea58847fa6bd2d931bcf08b8
Bijoy Sijo <bijoysijo21@gmail.com>
2020-08-05 10:29:13 +0530

    fix namespace class issue

--- app/models/weekly_review.rb
+++ app/models/weekly_review.rb
@@ -1,6 +1,5 @@
 class WeeklyReview < ApplicationRecord
-  has_many :review_question, class_name: WeeklyReview::ReviewQuestion
-  has_many :weekly_review_question, through: :review_question, class_name: WeeklyReview::Question
-  has_many :weekly_review_answer, class_name: WeeklyReview::Answer
+  has_many :review_question, class_name: 'WeeklyReview::ReviewQuestion'
+  has_many :weekly_review_question, through: :review_question, class_name: 'WeeklyReview::Question'
+  has_many :weekly_review_answer, class_name: 'WeeklyReview::Answer'
 end
-
